9c7b9e0ba0f86f33abb8cd4530da44e6b746b1fb
Michael Schoonmaker <michael.r.schoonmaker@gmail.com>
2019-02-27 17:14:15 -0800

    Add test for two-request plain text files.

--- parsers/text_test.go
+++ parsers/text_test.go
@@ -46,3 +46,41 @@ func TestLoadEmpty(t *testing.T) {
 	assert.NotNil(spec)
 	assert.Equal(0, len(spec.Tests))
 }
+
+func TestLoadDouble(t *testing.T) {
+	assert := assert.New(t)
+	subject := parsers.PlainTextParser{}
+	body := loaders.Body{
+		Lines: []string{
+			"> METHOD path",
+			"> Header: Request",
+			"< PROTO 1337 STATUS TEXT",
+			"< Header: Response",
+			"> SECOND path",
+			"> Header: Request 2",
+			"< PROTO 1234 AGAIN",
+			"< Header: Response 2",
+		},
+	}
+
+	spec, err := subject.Parse(&body)
+	assert.Nil(err)
+	assert.NotNil(spec)
+	assert.Equal(2, len(spec.Tests))
+
+	test := spec.Tests[0]
+	assert.Equal("METHOD", test.Request.Method)
+	assert.Equal("path", test.Request.Path)
+	assert.Equal("Request", test.Request.Headers["Header"])
+	assert.Equal(1337, test.Response.StatusCode)
+	assert.Equal("STATUS TEXT", test.Response.StatusText)
+	assert.Equal("Response", test.Response.Headers["Header"])
+
+	test = spec.Tests[1]
+	assert.Equal("SECOND", test.Request.Method)
+	assert.Equal("path", test.Request.Path)
+	assert.Equal("Request 2", test.Request.Headers["Header"])
+	assert.Equal(1234, test.Response.StatusCode)
+	assert.Equal("AGAIN", test.Response.StatusText)
+	assert.Equal("Response 2", test.Response.Headers["Header"])
+}

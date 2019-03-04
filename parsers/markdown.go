d5d06f497bcd0e75773d4a881e25593f96ce2202
Michael Schoonmaker <michael.r.schoonmaker@gmail.com>
2019-03-04 15:00:48 -0800

    Name tests from Markdown headers.
    Before tests from Markdown files were named with the fallback.
    Now tests from Markdown files are named with the last header.
    
    If a header is not found, the fallback is still used.

--- parsers/markdown.go
+++ parsers/markdown.go
@@ -1,9 +1,58 @@
 package parsers
 
-import "github.com/testdouble/http-assertion-tool/loaders"
+import (
+	"strings"
 
-type MarkdownParser struct{}
+	"github.com/testdouble/http-assertion-tool/loaders"
+)
 
-func (m *MarkdownParser) Parse(*loaders.Body) (*Spec, error) {
-	return nil, nil
+type Markdown struct {
+	plainText PlainTextParser
+}
+
+const (
+	InRichText = iota
+	InCodeFence
+)
+
+func (m *Markdown) Parse(body *loaders.Body) (*Spec, error) {
+	mode := InRichText
+	thisTestName := ""
+	nextTestName := ""
+	state := newParserState()
+	state.finalizer = func(test *Test) {
+		if len(thisTestName) == 0 {
+			fallbackTestName(test)
+		} else {
+			test.Name = thisTestName
+		}
+
+		thisTestName = nextTestName
+		nextTestName = ""
+	}
+
+	for _, line := range body.Lines {
+		trimmedLine := strings.TrimSpace(line)
+
+		switch {
+		case strings.HasPrefix(trimmedLine, "```"):
+			if mode == InRichText {
+				mode = InCodeFence
+			} else {
+				mode = InRichText
+			}
+		case mode == InCodeFence:
+			state.addLine(line)
+		case strings.HasPrefix(trimmedLine, "#"):
+			name := strings.TrimSpace(strings.SplitN(trimmedLine, " ", 2)[1])
+
+			if state.mode == ModeAwaitingRequest {
+				thisTestName = name
+			} else {
+				nextTestName = name
+			}
+		}
+	}
+
+	return state.finalize()
 }

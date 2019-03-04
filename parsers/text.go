4b1f3c9496d7b901484205238a1141ee5c479d1f
Michael Schoonmaker <michael.r.schoonmaker@gmail.com>
2019-03-04 14:30:01 -0800

    Extract shareable code from Text parser.
    This allows future parsers like Markdown to share the core parsing
    code. This should be further refactored, and out of the text submodule.

--- parsers/text.go
+++ parsers/text.go
@@ -19,75 +19,103 @@ const (
 
 type PlainTextParser struct{}
 
-func finalizeTest(test *Test) {
-	test.Name = fmt.Sprintf("%s %s -> %d", test.Request.Method, test.Request.Path, test.Response.StatusCode)
+type parserState struct {
+	currentTest Test
+	mode        int
+	spec        *Spec
 }
 
-func (m *PlainTextParser) Parse(body *loaders.Body) (*Spec, error) {
-	mode := AwaitingRequest
-	tests := make([]Test, 0)
-	currentTest := Test{}
-	for _, line := range body.Lines {
-		switch {
-		case len(line) == 0:
-			continue
-		case line[0] == '>':
-			line = strings.TrimSpace(line[1:])
+func newParserState() parserState {
+	spec := new(Spec)
+	spec.Tests = make([]Test, 0)
 
-			if mode == AwaitingRequest || mode == InResponseHeaders || mode == AwaitingResponseBody {
-				if mode == InResponseHeaders || mode == AwaitingResponseBody {
-					finalizeTest(&currentTest)
-					tests = append(tests, currentTest)
-					currentTest = Test{}
-				}
+	return parserState{
+		mode: AwaitingRequest,
+		spec: spec,
+	}
+}
 
-				currentTest.Request = RequestFromLine(line)
-				mode = InRequestHeaders
-			} else if mode == InRequestHeaders && len(line) == 0 {
-				mode = AwaitingRequestBody
-			} else if mode == InRequestHeaders {
-				key, value := HeaderFromLine(line)
-				currentTest.Request.Headers[key] = value
+func (s *parserState) pushCurrentTest() {
+	s.currentTest.Name = fmt.Sprintf(
+		"%s %s -> %d", s.currentTest.Request.Method, s.currentTest.Request.Path, s.currentTest.Response.StatusCode)
+
+	s.spec.Tests = append(s.spec.Tests, s.currentTest)
+	s.currentTest = Test{}
+}
+
+func (s *parserState) addLine(line string) error {
+	switch {
+	case len(line) == 0:
+		return nil
+	case line[0] == '>':
+		line = strings.TrimSpace(line[1:])
+
+		if s.mode == AwaitingRequest || s.mode == InResponseHeaders || s.mode == AwaitingResponseBody {
+			if s.mode == InResponseHeaders || s.mode == AwaitingResponseBody {
+				s.pushCurrentTest()
 			}
-		case line[0] == '<':
-			line = strings.TrimSpace(line[1:])
 
-			if mode == InRequestHeaders || mode == AwaitingRequestBody {
-				currentTest.Response = ResponseFromLine(line)
-				mode = InResponseHeaders
-			} else if mode == InResponseHeaders && len(line) == 0 {
-				mode = AwaitingResponseBody
-			} else if mode == InResponseHeaders {
-				key, value := HeaderFromLine(line)
-				currentTest.Response.Headers[key] = value
-			}
+			s.currentTest.Request = RequestFromLine(line)
+			s.mode = InRequestHeaders
+		} else if s.mode == InRequestHeaders && len(line) == 0 {
+			s.mode = AwaitingRequestBody
+		} else if s.mode == InRequestHeaders {
+			key, value := HeaderFromLine(line)
+			s.currentTest.Request.Headers[key] = value
+		}
+	case line[0] == '<':
+		line = strings.TrimSpace(line[1:])
+
+		if s.mode == InRequestHeaders || s.mode == AwaitingRequestBody {
+			s.currentTest.Response = ResponseFromLine(line)
+			s.mode = InResponseHeaders
+		} else if s.mode == InResponseHeaders && len(line) == 0 {
+			s.mode = AwaitingResponseBody
+		} else if s.mode == InResponseHeaders {
+			key, value := HeaderFromLine(line)
+			s.currentTest.Response.Headers[key] = value
 		}
 	}
 
-	if mode != AwaitingRequest {
-		finalizeTest(&currentTest)
-		tests = append(tests, currentTest)
-	}
-
-	spec := Spec{
-		Tests: tests,
-	}
-
-	return &spec, nil
+	return nil
 }
 
+func (s *parserState) finalize() (*Spec, error) {
+	if s.mode != AwaitingRequest {
+		s.pushCurrentTest()
+	}
+
+	return s.spec, nil
+}
+
+func (m *PlainTextParser) Parse(body *loaders.Body) (*Spec, error) {
+	state := newParserState()
+
+	for _, line := range body.Lines {
+		err := state.addLine(line)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return state.finalize()
+}
+
+// TODO(schoon) - Handle badly-formatted lines.
 func RequestFromLine(line string) *http.Request {
 	pieces := strings.Split(line, " ")
 
 	return http.NewRequest(pieces[0], pieces[1])
 }
 
+// TODO(schoon) - Handle badly-formatted lines.
 func HeaderFromLine(line string) (string, string) {
 	pieces := strings.Split(line, ":")
 
 	return strings.TrimSpace(pieces[0]), strings.TrimSpace(pieces[1])
 }
 
+// TODO(schoon) - Handle badly-formatted lines.
 func ResponseFromLine(line string) *http.Response {
 	pieces := strings.Split(line, " ")
 	code, _ := strconv.Atoi(pieces[1])
